91cc62b2d8bf1b33337d512da28fdcbf679984c6
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-03-24 22:06:46 -0400

    fix imports

--- src/lambda_cloud/__init__.py
+++ src/lambda_cloud/__init__.py
@@ -5,14 +5,14 @@ A Python client for interacting with the Lambda Cloud API.
 """
 
 from .client import LambdaCloudClient
-from .filesystems import FilesystemAPI
-from .firewall import FirewallAPI
-from .images import ImageAPI
-from .instances import InstanceAPI
+from .filesystems import Filesystems
+from .firewall import FirewallRules
+from .images import Images
+from .instances import Instances
 from .models import *
-from .ssh_keys import SSHKeyAPI
+from .ssh_keys import SSHKeys
 
-__all__ = ["LambdaCloudClient", "InstanceAPI", "SSHKeyAPI", "FilesystemAPI", "ImageAPI", "FirewallAPI"]
+__all__ = ["LambdaCloudClient", "Instances", "SSHKeys", "Filesystems", "Images", "FirewallRules"]
 
 # Version of the lambda_cloud package
 __version__ = "0.1.0"
